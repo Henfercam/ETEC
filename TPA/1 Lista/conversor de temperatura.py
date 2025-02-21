372e707e1ad79eb81842c174b14d513f92b1cf9b
Henfercam <henrique.fernandes.campos@gmail.com>
2025-02-21 19:11:10 -0300

    arrumadinha das pastas

---
+++ TPA/1 Lista/conversor de temperatura.py
@@ -0,0 +1,3 @@
+f = int(input('Quantos fahrenheit são? '))
+c = float((f-32)*5/9)
+print(f'em celsius são {c}')
---
+++ TPA/1 Lista/desconto.py
@@ -0,0 +1,4 @@
+p = float(input('Qual o preço do produto? '))
+d = float(p * 0.07)
+t = float(p-d)
+print(f'O preço com desconto é {t}')
